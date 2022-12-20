4e4c73bfe520c5c76a361b2320872bf30ce1c90a
hclee <hclee@dataeum.kr>
2022-12-20 14:29:51 +0900

    .

---
+++ src/main/java/study/java/basic/practice/practice01.java
@@ -0,0 +1,14 @@
+package study.java.basic.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practice01 {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        System.out.println("list.size() = " + list.size());
+    }
+}
